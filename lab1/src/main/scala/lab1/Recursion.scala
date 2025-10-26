fe9ca61796659dc207172b9c5a8e86e74b83213b
justlanxuan <justlanxuan@gmail.com>
2025-10-26 15:23:06 +0800

    lab1

--- lab1/src/main/scala/lab1/Recursion.scala
+++ lab1/src/main/scala/lab1/Recursion.scala
@@ -15,22 +15,24 @@ object Recursion:
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = 
-    if c == 0 || r == c then 1
+    if c == 0 || c == r then 1
     else pascal(c-1,r-1)+pascal(c,r-1)
+    
+  
 
   /**
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = 
     @tailrec
-    def loop(chars: List[Char], acc: Int): Boolean = 
-      if acc<0 then false
-      else if chars.isEmpty&&acc==0 then true
-      else if chars.isEmpty then false
-      else if chars.head == '(' then loop(chars.tail, acc + 1)
-      else if chars.head == ')' then loop(chars.tail, acc - 1)
-      else loop(chars.tail, acc)
-    loop(chars, 0)
+    def loop(chars: List[Char],paras: Int): Boolean = chars match
+      case _ if paras < 0 => false
+      case '('::tail => loop(tail,paras + 1)
+      case ')'::tail => loop(tail, paras - 1)
+      case Nil if paras == 0 => true
+      case Nil => false
+      case _ => loop(chars.tail, paras)
+    loop(chars,0)
 
 
   /**
@@ -46,7 +48,11 @@ Hint: Think of the degenerate cases. How many ways can you give change for 0 HKD
 Hint: you can reuse the coin
    */
   def countChange(money: Int, coins: List[Int]): Int = 
-    if money == 0 then 1
-    else if money < 0 || coins.isEmpty then 0
-    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    if money < 0 then 0
+    else if money == 0 then 1
+    else coins match
+      case head::tail=>countChange(money-head,coins) + countChange(money,tail)
+      case Nil => 0
+    
+
 
